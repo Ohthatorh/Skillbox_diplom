bda1a5ff0af35144faa72af81854eb9cb5165083
ohthatorh <ohthatorh>
2020-12-26 17:59:14 +0300

    upd 26.12 scroll

--- js/script.js
+++ js/script.js
@@ -3,42 +3,48 @@ $(document).ready(function() {
 		if(document.documentElement.clientWidth > 1170) {
 			$('.hamburger--vortex').removeClass('is-active');
 			$('.header__nav').removeClass('header__nav-active');
-			$('body').removeClass('disable-scroll');
+			enableScroll();
 		}
 	});
 
+	$('.symbols__main').removeClass('symbols__main_active');
+
 	let disableScroll = function () {
 		let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
-		console.log(paddingOffset)
 		$('body').css('padding-right', paddingOffset);
-		
+		$('.fixed').css('padding-right', paddingOffset);
+		$('body').addClass('disable-scroll');
 	}
-	
-	$('.symbols__main').removeClass('symbols__main_active');
+
+	let enableScroll = function () {
+		let paddingOffset = '0px';
+		$('body').css('padding-right', paddingOffset);
+		$('.fixed').css('padding-right', paddingOffset);
+		$('body').removeClass('disable-scroll');
+	}
 	
 	$('.hamburger--vortex').click(function() {
 		$(this).toggleClass('is-active');
-		$('body').toggleClass('disable-scroll');
+		($('.hamburger--vortex').hasClass('is-active')) ? disableScroll() : enableScroll();
 		$('.header__nav').toggleClass('header__nav-active');
 	});
 
 	$('.button-js').click(function(){
-		$('.popup-overlay').fadeIn();
-		$('body').addClass('disable-scroll');
-		$('.popup').css('display', 'flex').hide().fadeIn();
 		disableScroll();
+		$('.popup-overlay').fadeIn();
+		$('.popup').css('display', 'flex').hide().fadeIn();
 	});
 
 	$('.popup__close, .popup-overlay').click(function(){
+		enableScroll();
 		$('.popup-overlay').fadeOut();
 		$('.popup').fadeOut();
-		$('body').removeClass('disable-scroll');
 	});
 
 	$('a.scroll-to').on('click', function(e){
 		e.preventDefault();
 		$('.hamburger--vortex').removeClass('is-active');
-		$('body').removeClass('disable-scroll');
+		enableScroll();
 		$('.header__nav').removeClass('header__nav-active');
 		let anchor = $(this).attr('href');
 		$('html, body').stop().animate({
