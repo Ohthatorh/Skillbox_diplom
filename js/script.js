03f4f0668124a0d7ee8e8fc11bb717168962a87b
ohthatorh <ohthatorh>
2020-12-29 17:07:54 +0300

    upd 29.12

--- js/script.js
+++ js/script.js
@@ -9,18 +9,23 @@ $(document).ready(function() {
 
 	$('.symbols__main').removeClass('symbols__main_active');
 
-	let disableScroll = function () {
-		let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
-		$('body').css('padding-right', paddingOffset);
+	let disableScroll = () => {
+		let paddingOffset = $(window).innerWidth() - $('body').outerWidth() + 'px';
+		let pagePosition = window.scrollY;
+		$('body').css({'padding-right': paddingOffset, 'top': -pagePosition+'px'});
 		$('.fixed').css('padding-right', paddingOffset);
 		$('body').addClass('disable-scroll');
+		document.body.dataset.position = pagePosition;
 	}
 
-	let enableScroll = function () {
+	let enableScroll = () => {
 		let paddingOffset = '0px';
-		$('body').css('padding-right', paddingOffset);
+		let pagePosition = parseInt(document.body.dataset.position, 10);
+		$('body').css({'padding-right': paddingOffset, 'top': 'auto'});
 		$('.fixed').css('padding-right', paddingOffset);
 		$('body').removeClass('disable-scroll');
+		window.scroll({top: pagePosition, left: 0});
+		$('body').removeAttr('data-position');
 	}
 	
 	$('.hamburger--vortex').click(function() {
@@ -31,22 +36,23 @@ $(document).ready(function() {
 
 	$('.button-js').click(function(){
 		disableScroll();
+		$('.popup-overlay').css('top', -window.scrollY+'px');
 		$('.popup-overlay').fadeIn();
 		$('.popup').css('display', 'flex').hide().fadeIn();
 	});
 
 	$('.popup__close, .popup-overlay').click(function(){
-		enableScroll();
 		$('.popup-overlay').fadeOut();
 		$('.popup').fadeOut();
+		setTimeout(enableScroll, 800);
 	});
 
 	$('a.scroll-to').on('click', function(e){
+		let anchor = $(this).attr('href');
 		e.preventDefault();
 		$('.hamburger--vortex').removeClass('is-active');
 		enableScroll();
 		$('.header__nav').removeClass('header__nav-active');
-		let anchor = $(this).attr('href');
 		$('html, body').stop().animate({
 			scrollTop: $(anchor).offset().top
 		}, 800);
